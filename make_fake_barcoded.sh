582070c48e538067851f3cdd0d9f31fe058723c4
Kevin Murray <spam@kdmurray.id.au>
2016-02-14 00:24:46 -0800

    remove old scripts

--- make_fake_barcoded.sh
+++
@@ -1,4 +0,0 @@
-
-python fakebcd.py ath_fake_novar_1M.ilfq >ath_fake_novar_1M_barcoded.ilfq 2>ath_fake_novar_1M_barcoded.barcodes
-
-paste <(cut -f 1 ath_fake_novar_1M_barcoded.barcodes |grep -v None) <(for i in {1..10}; do echo $i; done) > fake_barcodes.axe
